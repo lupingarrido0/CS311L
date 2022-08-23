c17809400e33fb4422c760f9df5ca51fa5215aa0
Lupin Garrido <2211689@slu.edu.ph>
2022-08-24 07:03:45 +0800

    Growth of a Population 7kyu Test Completed

---
+++ src/Arge.java
@@ -0,0 +1,12 @@
+public class Arge {
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int years = 0;
+        int inhabitants = p0;
+        while(inhabitants < p)
+        {
+            inhabitants = (int)(inhabitants + (inhabitants * (float)(percent/100)) + aug);
+            years++;
+        }
+        return years;
+    }
+}
