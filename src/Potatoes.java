16208d20be0b02870c8823a2c865195c00f497c0
Lupin Garrido <2211689@slu.edu.ph>
2022-08-30 21:55:13 +0800

    Potatoes and ReversedStrings Test Completed

---
+++ src/Potatoes.java
@@ -0,0 +1,5 @@
+public class Potatoes {
+    public static int potatoes(int p0, int w0, int p1) {
+        return w0 * (100-p0)/(100-p1);
+    }
+}
---
+++ src/ReversedStrings.java
@@ -0,0 +1,6 @@
+public class ReversedStrings {
+    public static String solution(String str) {
+        // your code here...
+        return new StringBuilder(str).reverse().toString();
+    }
+}
