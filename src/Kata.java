8d1990a7012072e92677fd158d76792c6d62f90e
Rikoen <2211694@slu.edu.ph>
2022-08-30 09:32:56 +0800

    Input Kata.java & MexicanWave.java

--- src/Kata.java
+++ src/Kata.java
@@ -1,5 +1,13 @@
 public class Kata {
+
   public static int makeNegative(final int x) {
-    return x > 0 ? 0 - x: x;
+
+    if (x <= 0){
+      return x;
+    } else {
+      return -1*x;
+    }
+
   }
+
 }
--- src/MexicanWave.java
+++ src/MexicanWave.java
@@ -1,1 +1,24 @@
+public class MexicanWave {
 
+    public static String[] wave(String mString) {
+        mString = mString.toLowerCase();
+        int seatCount = countSeat(mString);
+        String[] wave = new String[seatCount];
+        for (int waveIndex = 0, seatIndex = 0; waveIndex < wave.length; waveIndex++, seatIndex++) {
+            char seat = mString.charAt(seatIndex);
+            while (Character.isWhitespace(seat)) {
+                seat = mString.charAt(++seatIndex);
+            }
+            StringBuilder mWaveStringBuilder = new StringBuilder(mString);
+            mWaveStringBuilder.setCharAt(seatIndex, Character.toUpperCase(seat));
+            wave[waveIndex] = mWaveStringBuilder.toString();
+        }
+        return wave;
+    }
+
+    private static int countSeat(String seatString) {
+        int mSpaceCount = (int) seatString.codePoints().filter(Character::isWhitespace).count();
+        return seatString.length() - mSpaceCount;
+    }
+
+}
